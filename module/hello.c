16e81cf7348e62ecf37c855dd90780b1cca2c96c
Dennis Cornwell <cornwe19@gmail.com>
2013-03-18 16:05:27 -0400

    Switching over to using intel device ID mappng verbatim so that we can get the right number of bdbars per device.

--- module/hello.c
+++ module/hello.c
@@ -8,41 +8,63 @@
 
 #define print812(...) printk( "<1>812 " ); printk( __VA_ARGS__ ); printk( "\n" );
 
+MODULE_AUTHOR( "Brett Lesnau, Dennis Cornwell" );
 MODULE_LICENSE( "Dual BSD/GPL" );
 
-enum { DEVICE_SIMPLE };
+enum { DEVICE_INTEL, DEVICE_INTEL_ICH4, DEVICE_SIS, DEVICE_NFORCE, DEVICE_ALI };
 
-static struct pci_device_id snd_simple_ids[] =  {
-	{ PCI_VDEVICE(INTEL, 0x2415), DEVICE_SIMPLE },	/* 82801AA */
-	{ PCI_VDEVICE(INTEL, 0x2425), DEVICE_SIMPLE },	/* 82901AB */
-	{ PCI_VDEVICE(INTEL, 0x2445), DEVICE_SIMPLE },	/* 82801BA */
-	{ PCI_VDEVICE(INTEL, 0x2485), DEVICE_SIMPLE },	/* ICH3 */
-	{ PCI_VDEVICE(INTEL, 0x24c5), DEVICE_SIMPLE }, /* ICH4 */
-	{ PCI_VDEVICE(INTEL, 0x24d5), DEVICE_SIMPLE }, /* ICH5 */
-	{ PCI_VDEVICE(INTEL, 0x25a6), DEVICE_SIMPLE }, /* ESB */
-	{ PCI_VDEVICE(INTEL, 0x266e), DEVICE_SIMPLE }, /* ICH6 */
-	{ PCI_VDEVICE(INTEL, 0x27de), DEVICE_SIMPLE }, /* ICH7 */
-	{ PCI_VDEVICE(INTEL, 0x2698), DEVICE_SIMPLE }, /* ESB2 */
-	{ PCI_VDEVICE(INTEL, 0x7195), DEVICE_SIMPLE },	/* 440MX */
-	{ PCI_VDEVICE(SI, 0x7012), DEVICE_SIMPLE },	/* SI7012 */
-	{ PCI_VDEVICE(NVIDIA, 0x01b1), DEVICE_SIMPLE },	/* NFORCE */
-	{ PCI_VDEVICE(NVIDIA, 0x003a), DEVICE_SIMPLE },	/* MCP04 */
-	{ PCI_VDEVICE(NVIDIA, 0x006a), DEVICE_SIMPLE },	/* NFORCE2 */
-	{ PCI_VDEVICE(NVIDIA, 0x0059), DEVICE_SIMPLE },	/* CK804 */
-	{ PCI_VDEVICE(NVIDIA, 0x008a), DEVICE_SIMPLE },	/* CK8 */
-	{ PCI_VDEVICE(NVIDIA, 0x00da), DEVICE_SIMPLE },	/* NFORCE3 */
-	{ PCI_VDEVICE(NVIDIA, 0x00ea), DEVICE_SIMPLE },	/* CK8S */
-	{ PCI_VDEVICE(NVIDIA, 0x026b), DEVICE_SIMPLE },	/* MCP51 */
-	{ PCI_VDEVICE(AMD, 0x746d), DEVICE_SIMPLE },	/* AMD8111 */
-	{ PCI_VDEVICE(AMD, 0x7445), DEVICE_SIMPLE },	/* AMD768 */
-	{ PCI_VDEVICE(AL, 0x5455), DEVICE_SIMPLE },   /* Ali5455 */
+static struct pci_device_id intel_dev_ids[] = {
+	{ PCI_VDEVICE(INTEL, 0x2415), DEVICE_INTEL },	/* 82801AA */
+	{ PCI_VDEVICE(INTEL, 0x2425), DEVICE_INTEL },	/* 82901AB */
+	{ PCI_VDEVICE(INTEL, 0x2445), DEVICE_INTEL },	/* 82801BA */
+	{ PCI_VDEVICE(INTEL, 0x2485), DEVICE_INTEL },	/* ICH3 */
+	{ PCI_VDEVICE(INTEL, 0x24c5), DEVICE_INTEL_ICH4 }, /* ICH4 */
+	{ PCI_VDEVICE(INTEL, 0x24d5), DEVICE_INTEL_ICH4 }, /* ICH5 */
+	{ PCI_VDEVICE(INTEL, 0x25a6), DEVICE_INTEL_ICH4 }, /* ESB */
+	{ PCI_VDEVICE(INTEL, 0x266e), DEVICE_INTEL_ICH4 }, /* ICH6 */
+	{ PCI_VDEVICE(INTEL, 0x27de), DEVICE_INTEL_ICH4 }, /* ICH7 */
+	{ PCI_VDEVICE(INTEL, 0x2698), DEVICE_INTEL_ICH4 }, /* ESB2 */
+	{ PCI_VDEVICE(INTEL, 0x7195), DEVICE_INTEL },	/* 440MX */
+	{ PCI_VDEVICE(SI, 0x7012), DEVICE_SIS },	/* SI7012 */
+	{ PCI_VDEVICE(NVIDIA, 0x01b1), DEVICE_NFORCE },	/* NFORCE */
+	{ PCI_VDEVICE(NVIDIA, 0x003a), DEVICE_NFORCE },	/* MCP04 */
+	{ PCI_VDEVICE(NVIDIA, 0x006a), DEVICE_NFORCE },	/* NFORCE2 */
+	{ PCI_VDEVICE(NVIDIA, 0x0059), DEVICE_NFORCE },	/* CK804 */
+	{ PCI_VDEVICE(NVIDIA, 0x008a), DEVICE_NFORCE },	/* CK8 */
+	{ PCI_VDEVICE(NVIDIA, 0x00da), DEVICE_NFORCE },	/* NFORCE3 */
+	{ PCI_VDEVICE(NVIDIA, 0x00ea), DEVICE_NFORCE },	/* CK8S */
+	{ PCI_VDEVICE(NVIDIA, 0x026b), DEVICE_NFORCE },	/* MCP51 */
+	{ PCI_VDEVICE(AMD, 0x746d), DEVICE_INTEL },	/* AMD8111 */
+	{ PCI_VDEVICE(AMD, 0x7445), DEVICE_INTEL },	/* AMD768 */
+	{ PCI_VDEVICE(AL, 0x5455), DEVICE_ALI },   /* Ali5455 */
 	{ 0, }
 };
 
-static int __devinit snd_simple_create( struct pci_dev *pci, const struct pci_device_id *pci_id ) {
-   print812( "In device constructor with IRQ %d", pci->irq );
+// Mapping of nr bd bars per device ID
+#if 0
+static unsigned int bdbars[] = {
+    3, /* DEVICE_INTEL */
+    6, /* DEVICE_INTEL_ICH4 */
+    3, /* DEVICE_SIS */
+    6, /* DEVICE_ALI */
+    4, /* DEVICE_NFORCE */
+};
+#endif
 
-   return 0;
+static char* device_id_names[] = {
+    "DEVICE_INTEL",
+    "DEVICE_INTEL_ICH4",
+    "DEVICE_SIS",
+    "DEVICE_NFORCE",
+    "DEVICE_ALI"
+};
+
+static int __devinit snd_simple_create( struct pci_dev *pci, const struct pci_device_id *pci_id ) {
+    print812( "In device constructor with IRQ %d", pci->irq );
+  
+    print812( "Using device ID %ld (%s)", pci_id->driver_data, device_id_names[pci_id->driver_data] );   
+
+    return 0;
 }
 
 static void __devexit snd_simple_exit( struct pci_dev *pci ) {
@@ -55,7 +77,7 @@ static void __devexit snd_simple_exit( struct pci_dev *pci ) {
 
 static struct pci_driver driver = { 
    .name = "Simple Driver",
-   .id_table = snd_simple_ids,
+   .id_table = intel_dev_ids,
    .probe = snd_simple_create,
    // Wrap .remove to turn the exit function into a NULL pointer if
    // the driver is compiled into the kernel
