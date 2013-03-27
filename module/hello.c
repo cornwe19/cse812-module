9e117f472dae8fd88f3eb661de4070ae15b87148
Dennis Cornwell <cornwe19@gmail.com>
2013-03-27 16:54:51 -0400

    Changing hello.c into an IO device. Just stubbing things in for now.

--- module/hello.c
+++ module/hello.c
@@ -1,15 +1,65 @@
+#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h> /* printk() */
+#include <linux/slab.h> /* kmalloc() */
+#include <linux/fs.h> /* everything... */
+#include <linux/errno.h> /* error codes */
+#include <linux/types.h> /* size_t */
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h> /* O_ACCMODE */
+#include <asm/system.h> /* cli(), *_flags */
+#include <asm/uaccess.h> /* copy_from/to_user */
+
+#define print812(...) printk( "<1>812 " ); printk( __VA_ARGS__ ); printk( "\n" );
+
+#define KEYLOG_MAJOR 60
+#define KEYLOG_NAME  "keylog"
 
 MODULE_LICENSE( "Dual BSD/GPL" );
 
+static char *key_buffer;
+
+int key_open( struct inode *inode, struct file *filp );
+ssize_t key_read( struct file *filp, char *buf, size_t count, loff_t *f_pos );
+int key_release( struct inode *inode, struct file *filp );
+
+struct file_operations fops = {
+    read:    key_read,
+    open:    key_open,
+    release: key_release,
+};
+
 static int hello_init( void ) {
-    printk( "<1> Initializing the module." );
+    int err;
+    print812( "Initializing the module." );
+
+    err = register_chrdev( KEYLOG_MAJOR, KEYLOG_NAME, &fops );
+    if ( err < 0 ) {
+        print812( "Registering chrdev failed (%d)", err );
+        return err;
+    }
+    
+    return 0;
+}
+
+int key_open( struct inode *inode, struct file *filp ) {
+
+    return 0;
+}
+ssize_t key_read( struct file *filp, char *buf, size_t count, loff_t *f_pos ) {
+    
+    return 0;
+}
+
+int key_release( struct inode *inode, struct file *filp ) {
 
     return 0;
 }
 
 static void hello_exit( void ) {
     printk( "<1> Unregistering the module" );
+
+    unregister_chrdev( KEYLOG_MAJOR, KEYLOG_NAME );
 }
 
 module_init( hello_init );
