bc2b82c7f6de144cc79ed05d846990a399eaf487
BLesnau <lesnaubr@gmail.com>
2013-04-07 14:24:59 -0400

    Adding Skeleton Code For Testing Sockets

---
+++ socketTest/common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <string.h>
+
+#define PROGRAM_FAILURE 2
+#define DEFAULT_PROTOCOL 0 
+
+#define LOCAL_HOST "127.0.0.1"
+
+struct unicast_pkt {
+  unsigned int data;
+};
+
+#endif
---
+++ socketTest/send.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#include "common.h"
+#include "socket_utils.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "SEND" << endl;
+	return 0;
+}
